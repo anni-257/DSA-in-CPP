f74ea2c1c1073b2933b365462b08b63505e915a6
anni-257 <taraleaniket257@gmail.com>
2022-04-05 10:06:24 +0530

    Different approaches for factorial

--- Recursion/factorial.cpp
+++ Recursion/factorial.cpp
@@ -1,16 +1,32 @@
 #include<iostream>
 using namespace std;
 
-int factorial(int n){
+// Recursive approach
 
+int factorial(int n){
+	// for negative value recursion goes upto 262054 times --> avoid this neg,val
+	
 	if(n==0 || n==1)
 		return 1;
 		
 	return factorial(n-1)*n;
 }
 
+// Iterative approach
+
+int fact(int n){
+	int fact=1;
+	for(int i=2;i<=5;i++){
+		fact*=i;
+	}
+	return fact;
+}
+
+
+
 int main(){
 	int n=5;
-	cout<<"factorial of "<<n<<" is: "<<factorial(n)<<endl;
+	cout<<"Recursive: factorial of "<<n<<" is: "<<factorial(n)<<endl;
+	cout<<"Iterative: "<<fact(n)<<endl;
 	return 0;
 }
