b186f1475d92a2b6d2ab2c2132efd76eeb85f578
anni-257 <taraleaniket257@gmail.com>
2022-04-05 09:35:54 +0530

    Different approaches for sum of first n natural number

--- Recursion/sumOfNaturalNumbers.cpp
+++ Recursion/sumOfNaturalNumbers.cpp
@@ -1,14 +1,35 @@
 #include<iostream>
 using namespace std;
 
+// Recursive approach
+
 int sumOfNaturalNumbers(int n){
 	if(n>1){
 		return n+sumOfNaturalNumbers(n-1);
 	}
 	return 1;
 }
+
+int iterativeSum(int n){
+	int sum=0;
+	for(int i=1;i<=n;i++){
+		sum+=i;
+	}
+	
+	return sum;
+}
+
+// Direct Method
+
+int directSum(int n){
+	return n*(n+1)/2;
+}
+
+
 int main(){
 	int n=10;
-	cout<<"Sum of first "<<n<<" numbers is: "<<sumOfNaturalNumbers(n)<<endl;
+	cout<<"Recursive: Sum of first "<<n<<" numbers is: "<<sumOfNaturalNumbers(n)<<endl;
+	cout<<"Iterative: "<<iterativeSum(n)<<endl;
+	cout<<"Direct formula: "<<directSum(n)<<endl;
 	return 0;
 }
