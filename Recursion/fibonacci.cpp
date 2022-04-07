7bcdd4cf88718d12eb328eb004761a1893fbb7bf
anni-257 <taraleaniket257@gmail.com>
2022-04-07 14:52:17 +0530

    adding Memoization Technique

--- Recursion/fibonacci.cpp
+++ Recursion/fibonacci.cpp
@@ -26,11 +26,35 @@ int fibonacci(int n){
 	return s;
 }
 
+// Memorizarion
+
+int A[10]={-1};
+
+int Fibo(int n){
+	if(n<=1){
+		A[n]=n;
+		return n;
+	}
+	else{
+		if(A[n-1]==-1){
+			A[n-1]=Fibo(n-1);
+		}
+		
+		if(A[n-2]==-1){
+			A[n-2]=Fibo(n-2);
+		}
+		
+		A[n]=A[n-2]+A[n-1];
+	}
+	
+	return A[n];
+}
+
 int main(){
 
-	cout<<"Fibo 0: "<<fibonacci(0)<<endl;
-	cout<<"Fibo 1: "<<fibonacci(1)<<endl;
-	cout<<"Fibo 2: "<<fibonacci(2)<<endl;
-	cout<<"Fibo 3: "<<fibonacci(3)<<endl;
+	cout<<"Fibo 0: "<<Fibo(0)<<endl;
+	cout<<"Fibo 1: "<<Fibo(1)<<endl;
+	cout<<"Fibo 2: "<<Fibo(2)<<endl;
+	cout<<"Fibo 3: "<<Fibo(3)<<endl;
 	
 }
