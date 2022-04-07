a2b2a7e109d517f9941272f81e6cff0c33a835a9
anni-257 <taraleaniket257@gmail.com>
2022-04-07 14:19:31 +0530

    Recursive and Iterative Fibonacci

--- Recursion/fibonacci.cpp
+++ Recursion/fibonacci.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+//Recursive
 int fibo(int n){
 
 	if(n==0 || n==1){
@@ -10,11 +11,26 @@ int fibo(int n){
 	return fibo(n-1)+fibo(n-2);
 }
 
+//Iterative
+
+int fibonacci(int n){
+	int t0=0; t1=1; int s=0;
+	if(n<=1)
+		return n;
+	for(int i=2;i<=n;i++){
+		s=t0+t1;
+		t1=s;
+		t0=t1;
+	}
+	
+	return s;
+}
+
 int main(){
 
-	cout<<"Fibo 0: "<<fibo(0)<<endl;
-	cout<<"Fibo 1: "<<fibo(1)<<endl;
-	cout<<"Fibo 2: "<<fibo(2)<<endl;
-	cout<<"Fibo 3: "<<fibo(3)<<endl;
+	cout<<"Fibo 0: "<<fibonacci(0)<<endl;
+	cout<<"Fibo 1: "<<fibonacci(1)<<endl;
+	cout<<"Fibo 2: "<<fibonacci(2)<<endl;
+	cout<<"Fibo 3: "<<fibonacci(3)<<endl;
 	
 }
