39782be5ce8ff3d98e972bafff5176f543728f63
anni-257 <taraleaniket257@gmail.com>
2022-04-10 00:29:27 +0530

    Intersection of two sorted array

---
+++ Array_ADT/intersection.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include"Array.h"
+using namespace std;
+
+Array* Array::inter(Array *arr){
+	int i,j,k;
+	i=j=k=0;
+	Array *arr2=new Array(length);
+	arr2->A=new int[length];
+	while(i<length && j<arr->length){
+		if(A[i]<arr->A[j])
+			i++;
+		else if(arr->A[j]<A[i])
+			j++;
+		else {
+			arr2->A[k++]=A[i++];
+			j++;
+		}
+	
+	}
+	
+	arr2->length=k;
+	
+	return arr2;
+}
+
+int main(){
+	Array *arr=new Array(10,5);
+	Array *arr2=new Array(10,6);
+	Array *arr3;
+	cout<<"Array 1: "<<endl;
+	arr->display();
+	cout<<"Array 2: "<<endl;
+	arr2->display();
+	cout<<"intersection of two array: "<<endl;
+	arr3=arr->inter(arr2);
+	arr3->display();
+	return 0;
+	
+}
+
+
