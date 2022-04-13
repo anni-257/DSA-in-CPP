d26f7a9ea261ada1403727d3be22ed21ddac55f5
anni-257 <taraleaniket257@gmail.com>
2022-04-13 22:09:49 +0530

    Basics of char array

---
+++ String/basics.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+//Array of Character
+
+int main(){
+	char name[10]={'J','O','H','N'};
+	cout<<name<<endl;
+	cout<<"Enter Name: ";
+	cin>>name;
+	
+	cout<<"Name: "<<name<<endl;
+	
+	
+	return 0;
+
+}
