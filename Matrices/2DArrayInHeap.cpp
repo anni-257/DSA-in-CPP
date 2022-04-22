9f63951654a77f718d8527c84671c5298604495a
anni-257 <taraleaniket257@gmail.com>
2022-04-22 20:08:41 +0530

    2D-Array created in Heap

---
+++ Matrices/2DArrayInHeap.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int **A;
+	A=new int*[3];
+	
+	for(int i=0;i<3;i++){
+		A[i]=new int[4];
+	}
+	
+	cout<<"Enter Elements:";
+	for(int i=0;i<3;i++){
+		for(int j=0;j<4;j++){
+			cin>>A[i][j];
+		}
+	}
+	
+	cout<<"2D Matrix"<<endl;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<4;j++){
+			cout<<A[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	return 0;
+}
