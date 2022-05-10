6e1df963db2bc56197f3588b2e5791f11923f2d0
anni-257 <taraleaniket257@gmail.com>
2022-05-10 15:00:08 +0530

    Modified Node.h

--- LinkedList/Node.h
+++ LinkedList/Node.h
@@ -2,9 +2,9 @@
 using namespace std;
 
 class Node{
-	protected:
-		int val;
-		Node* next;
+	public:
+	int val;
+	Node* next;
 };
 
 class Link:public Node{
@@ -12,18 +12,53 @@ class Link:public Node{
 	public:
 	Link(){first=nullptr;}
 	Node* getNode(){return first;}
-	void create(int A[],int n);
+	void create(int *A, int n);
 	void display();
+	void rDisplay(Node*);
+	int countNode();
+	int rCountNode(Node*);
+	int sum();
+	int rSum(Node*);
+	int max();
+	int rMax(Node*);
+	int min();
+	int rMin(Node*);
+	Node* search(int key);
+	Node* rSearch(Node*,int key);
+	Node* improveSearch(int key);
+	void insert(int index,int x);
+	void insertLast(int x);
+	void sortedInsert(int x);
+	int Delete(int index);
+	void removeDuplicate();
+	void reverseLink();
+	void slidingPtrReverse();
+	void rReverse(Node*);
+	void concate(Node* q);
+	Node* merge(Node* q);
+	bool isLoop();
+	void circularDisplay();
+	void RCircularDisplay(Node*);
+	void insertCircular(int index, int x);
+	int circularLength();
+	void deleteCircularElement();
+	void doublyCreate(int *A, int n);
+	void doublyInsert(int index, int x);
+	int deleteDoublyLink(int index);
+	void doublyDisplay();
+	void reverseDoubly();
+	void displayReverseDoubly();
+	void insertCircularDoublyLink();
+	int doublyCircularCount();
 };
 
-void Link::create(int A[], int n){
-	Node* temp, *newnode;
-	
+void Link::create(int *A, int n){
+	Node* newnode, *temp;
+	if(n){
 	first=new Node;
 	first->val=A[0];
 	first->next=NULL;
 	temp=first;
-	
 	for(int i=1;i<n;i++){
 		newnode=new Node;
 		newnode->val=A[i];
@@ -31,10 +66,11 @@ void Link::create(int A[], int n){
 		temp->next=newnode;
 		temp=newnode;
 	}
+	}
 }
 
 void Link::display(){
-	Node *p=first;
+	Node* p=first;
 	while(p){
 		cout<<p->val<<"->";
 		p=p->next;
