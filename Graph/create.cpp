9bfe1e8c71cf54db5e0fb4285d7ce664c867a793
anni-257 <taraleaniket257@gmail.com>
2022-06-15 23:31:35 +0530

    create graph

---
+++ Graph/create.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void adj_Matrix(){
+	int n,m;
+	cin>>n>>m;
+	vector<vector<int>>adj(n+1,vector<int> ); 
+	for(int i=0;i<m;++i){
+		int u,v;
+		cin>>u>>v;
+		
+	}	
+	
+}
+
+int main(){
+
+	return 0;
+}
